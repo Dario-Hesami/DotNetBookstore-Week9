3da2dbbd4ae2b416b097bae9ed3704e7d259bcd4
Dario Hesami <Dario.Hesami@DESKTOP-4SPMNKA>
2023-11-02 09:08:07 -0400

    W9 - Security I - Authentication

--- DotNetBookstore/Controllers/CategoriesController.cs
+++ DotNetBookstore/Controllers/CategoriesController.cs
@@ -11,7 +11,8 @@ using DotNetBookstore.Models;
 using Microsoft.AspNetCore.Authorization;
 namespace DotNetBookstore.Controllers
 {
-    //[Authorize]
+    // make all methods private
+    [Authorize]
     public class CategoriesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -30,7 +31,7 @@ namespace DotNetBookstore.Controllers
         }
 
         // GET: Categories/Details/5
-        //[AllowAnonymous]
+        [AllowAnonymous]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null || _context.Categories == null)
